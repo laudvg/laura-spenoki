02787c93d430b10e87cad6df92400c31b83fb7e0
Laura <laura.vivas.gamboa@gmail.com>
2022-05-30 22:38:05 +0200

    This adds index and the PostCard component that shows all the posts

---
+++ components/MainBlog/PostCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <nuxt-link :to="`posts/` + id">
+      <b-card>
+        <h3>{{ title }}</h3> 
+        <h6>{{ id }}</h6>
+        <b-card-text>
+          {{ post }}
+        </b-card-text>
+      </b-card>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  name: "PostCard",
+  props: [
+    "post",
+    "id",
+    "title",
+    "user",
+  ],
+}
+</script>
+
+<style>
+
+</style>
--- pages/Posts/index.vue
+++ pages/Posts/index.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-    posts
+    <post-card v-for="post in posts" v-bind:key="post.id" 
+    :id="post.id"
+    :post="post.body"
+    :title="post.title"
+    :user="post.userId"
+    ></post-card>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import PostCard from "../../components/MainBlog/PostCard.vue";
 
 export default {
+  components: {
+    PostCard,
+  },
   data() {
     return {
       posts: [],
@@ -15,7 +24,7 @@ export default {
   },
   head() {
     return {
-      title: "About the blog",
+      title: "TMG Posts",
       meta: [
         {
           hid:"description", //unique identifier
@@ -31,9 +40,9 @@ export default {
       'Content-type': 'application/json; charset=UTF-8',
       },
     }
-    const posts = `https://jsonplaceholder.typicode.com/posts/`
     try {
-      const response = await axios.get(posts, configuration);
+      const route = `https://jsonplaceholder.typicode.com/posts/`
+      const response = await axios.get(route, configuration);
       this.posts = response.data
       // console.log(response.data);
     } catch (error) {
