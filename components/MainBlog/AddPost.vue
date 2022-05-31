ba7d61d30c2f960e2cabde6100e183d3041bf9be
Laura <laura.vivas.gamboa@gmail.com>
2022-05-31 05:23:07 +0200

    This adds some styling and also converts the input for the new blog in a modal

--- components/MainBlog/AddPost.vue
+++ components/MainBlog/AddPost.vue
@@ -1,34 +1,46 @@
 <template>
   <div>
-    <b-form-group
-      label-cols-lg="3"
-      label="New Group"
-      label-size="lg"
-      label-class="font-weight-bold pt-0"
-      class="mb-0"
-      @submit.prevent="addPosts"
-      method="post"
-    >
-      <b-form-group
-        label="Title:"
-        label-for="title"
-        label-cols-sm="3"
-        label-align-sm="right"
-      >
-        <b-form-input id="title" v-model="newPostData.title"></b-form-input>
-      </b-form-group>
-      <b-form-group
-        label="Body:"
-        label-for="body"
-        label-cols-sm="3"
-        label-align-sm="right"
-      >
-        <b-form-input id="body" v-model="newPostData.body"></b-form-input>
-      </b-form-group>
-      <b-button @click="addPosts">
-        Create New Post
-      </b-button>
-    </b-form-group>
+    <b-row>
+      <b-col cols="10"></b-col>
+      <b-modal 
+      id="modal-1" 
+      title="Add a new Post"
+      @ok="addPosts" 
+      size="xl" 
+      class="post-modal mt-4">
+        <b-form-group
+          label-cols-lg="3"
+          label="New Post"
+          label-size="lg"
+          label-class="font-weight-bold pt-0"
+          class="mb-0"
+          @submit.prevent="addPosts"
+          method="post"
+        >
+          <b-form-group
+            label="Title:"
+            label-for="title"
+            label-cols-sm="3"
+            label-align-sm="right"
+          >
+            <b-form-input id="title" v-model="newPostData.title"></b-form-input>
+          </b-form-group>
+          <b-form-group
+            label="Body:"
+            label-for="body"
+            label-cols-sm="3"
+            label-align-sm="right"
+          >
+            <b-form-textarea id="body" class="post-body" v-model="newPostData.body"></b-form-textarea>
+          </b-form-group>
+        </b-form-group>
+      </b-modal>
+      <b-col cols="2" align="right">
+        <b-button variant="light" class="mr-4" v-b-modal.modal-1>
+          Add New Post
+        </b-button>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
@@ -64,4 +76,11 @@ export default {
 
 <style>
 
+.post-body{
+  min-height: 30rem;
+}
+
+.modal-content{
+  margin-top: 6rem;
+}
 </style>
