a5b5e874f82e823e4cf4f8722d081f7b71af3f75
Laura <laura.vivas.gamboa@gmail.com>
2022-05-31 03:05:12 +0200

    This changes the default page and adds the header to it

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <BlogHeader/>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import BlogHeader from '../components/MainBlog/BlogHeader.vue'
+export default {
+layout: 'blog',
+components:{
+  BlogHeader,
+}
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,7 @@
 <template>
+<div>
+  Laura
+</div>
 </template>
 
 <script>
