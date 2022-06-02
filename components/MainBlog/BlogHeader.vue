52266bbacc8daabbba9bc9f1dfa90a115ac123ad
Laura <laura.vivas.gamboa@gmail.com>
2022-06-02 02:16:10 +0200

    This adds styles for the Comment Card and Blog Header

--- components/MainBlog/BlogHeader.vue
+++ components/MainBlog/BlogHeader.vue
@@ -3,6 +3,9 @@
     <div class="my-4">
       <h1 class="header-text my-0 ml-2">The Midnight <br/>Gospel</h1>
     </div>
+    <div class="my-4">
+      <hr class="header-hr"/>
+    </div>
   </div>
 </template>
 <script>
@@ -15,12 +18,20 @@
 
 .header-text{
   font-family: Oswald;
-  font-size: 180px;
-  text-shadow: 2px 4px yellow;
+  font-size: 120px;
+  text-shadow: 2px 4px #FFC107;
 }
 
 .header-subtitle{
   font-family: Roboto;
   font-size: 20px;
+}
+
+.header-hr{
+  margin-right: 5rem;
+  height: 4px;
+  color: #FFC107;
+  background-color: #FFC107;
+  border: none;
 }
 </style>
--- components/MainBlog/CommentCard.vue
+++ components/MainBlog/CommentCard.vue
@@ -1,8 +1,8 @@
 <template>
-  <b-card>
-    <h3>{{ name }}</h3> 
-    <h6>{{ email }}</h6>
-    <b-card-text>
+  <b-card class="comment-card">
+    <h6 class="user-name text-muted">{{ email }} says:</h6>
+    <h3 class="comment-name my-3">{{ name }}</h3> 
+    <b-card-text class="comment-body">
       {{ body }}
     </b-card-text>
   </b-card>
@@ -20,5 +20,21 @@ export default {
 </script>
 
 <style>
+.comment-card{
+  width:40rem;
+}
+.user-name{
+  font-family: Roboto;
+  font-size: .75rem;
+}
+.comment-name{
+  font-family: Roboto;
+  font-weight: 500;
+  font-size: 1rem;
+}
+.comment-body{
+  font-family: Roboto;
+  font-size: .75rem;
+}
 
 </style>
